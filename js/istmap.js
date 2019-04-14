54ca0ff2a15a5872a6ac97e8fc48b3d0323e7dab
nanlar <46804475+nanlar@users.noreply.github.com>
2019-04-14 16:11:26 -0500

    Used new Google API to center the map and put a marker to Istanbul.

--- js/istmap.js
+++ js/istmap.js
@@ -1,12 +1,25 @@
 /* Nilufer Anlar, istmap.js */
 
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
+// my javascript for map
 function initMap(){ 
 
-var map = new google.maps.Map(document.getElementById("istmap"), {
+  // the location of Istanbul as obtained from Google Maps
+  var ist = {lat: 41.015161, lng: 28.939397};
 
-     /* 6 digits on lat and long work as below */
-     center: {lat: 41.015161, lng: 28.939397},
+  // the map, centered at Istanbul
+  var map = new google.maps.Map(document.getElementById("istmap"), {zoom: 8, center: ist});
+      
+  // marker, positioned at istanbul
+  var marker = new google.maps.Marker({position: ist, map: map});
 
-     zoom: 8    });
 
 }
+
+// do I still need the function loaded as below??
+google.maps.event.addDomListener(window, "load", initMap);
