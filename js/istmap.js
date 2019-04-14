38ea2242885fa22e16b2d43a3ad82944d5a751d8
nanlar <46804475+nanlar@users.noreply.github.com>
2019-04-14 12:13:43 -0500

    Created the istmap.js file to contain the Istanbul map.

---
+++ js/istmap.js
@@ -0,0 +1,13 @@
+
+/* April 13 - Prof K. wants us to add the following code to this file */
+
+function initMap(){ 
+
+var map = new google.maps.Map(document.getElementById("istmap"), {
+
+     /* 6 digits on lat and long work as below */
+     center: {lat: 41.015161, lng: 28.939397},
+
+     zoom: 8    });
+
+}
