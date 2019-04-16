60e1f7624d7008e7554b7ed3f8b8bac948406c65
nanlar <46804475+nanlar@users.noreply.github.com>
2019-04-16 13:02:19 -0500

    Improved the popup text for the map marker, and some style changes.

--- js/istmap.js
+++ js/istmap.js
@@ -19,7 +19,7 @@ function initMap(){
   var marker = new google.maps.Marker({position: ist, map: map, title: "My Hometown Istanbul"});
 
   // add some text about the place 
-  var contentString = "Istanbul is the most populous city in Turkey and the country's economic, cultural and historic center." + " With a total population of around 15 million residents in its metropolitan area, it is also one of the world's most populous cities, ranking as the world's fourth largest city proper and the largest European city.";
+  var contentString = "<h3>Istanbul is the most populous city in Turkey and the country's economic, cultural and historic center.</h3>" + "<p>With a total population of around 15 million residents in its metropolitan area, it is also one of the world's most populous cities, ranking as the world's fourth largest city proper and the largest European city.</p>";
             
   var infowindow = new google.maps.InfoWindow({content: contentString});
   
